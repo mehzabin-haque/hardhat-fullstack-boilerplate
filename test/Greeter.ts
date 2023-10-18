80d32865cbdf18f3aef5d0d92adce2b4eab53d36
Tahlil <bsse0803@iit.du.ac.bd>
2023-10-18 19:08:41 +0600

    Simplify testing

--- test/Greeter.ts
+++ test/Greeter.ts
@@ -1,40 +1,15 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import { ethers, waffle} from 'hardhat';
-import GreeterArtifact from '../artifacts/contracts/Greeter.sol/Greeter.json';
-import {Greeter} from '../typechain/Greeter';
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
 
-const {deployContract} = waffle;
+describe('greeter', () => {
+	it("should return the new greeting once it's changed", async () => {
+		const Greeter = await ethers.getContractFactory('Greeter');
+		const greeter = await Greeter.deploy('Hello, world!');
 
-describe("Greeter test", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshopt in every test.
-  async function deployOnceFixture() {
-    let greeter: Greeter;
-    // Contracts are deployed using the first signer/account by default
-    const [owner, ...otherAccounts] = await ethers.getSigners();
+		await greeter.deployed();
+		expect(await greeter.greet()).to.equal('Hello, world!');
 
-    greeter = (await deployContract(owner, GreeterArtifact, ['Hello, world!'])) as Greeter;
-
-    return { greeter, owner, otherAccounts };
-  }
-
-  describe("Test suite", function () {
-    it("Test 1", async function () {
-      const { greeter, owner } = await loadFixture(deployOnceFixture);
-      expect(true);
-    });
-
-    it("Test 2", async function () {
-      const { greeter, owner } = await loadFixture(deployOnceFixture);
-      expect(1).to.equal(1);
-    });
-
-
-
-  });
-
- 
+		await greeter.setGreeting('Hola, mundo!');
+		expect(await greeter.greet()).to.equal('Hola, mundo!');
+	});
 });
